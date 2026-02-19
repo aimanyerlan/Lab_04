6d8b2b3fca3473fe00ef360f1a3629def71c06ba
aimanyerlan <yerlanaiman@mail.ru>
2026-02-19 06:46:48 +0500

    Fix: remove submodule structure

---
+++ task1/src/RegistrationForm.js
@@ -0,0 +1,6 @@
+import React, {useState} from "react";
+function RegistrationForm() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [age, setAge] = useState("");
+}
